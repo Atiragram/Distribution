36309a03095b29ba39fad3aa8c97405a9f3ac821
Elorfin <penin.axel@gmail.com>
2017-06-26 11:18:24 +0200

    adds react-router-dom + adds easy routed app

---
+++ main/core/Resources/modules/utilities/app/router.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import {PropTypes as T, checkPropTypes} from 'prop-types'
+import {
+  hashHistory,
+  HashRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+// if you use connected components, you may need to tell them there is a router.
+// @see https://reacttraining.com/react-router/web/guides/redux-integration
+
+const routesTypes = T.arrayOf(T.shape({
+  path: T.string,
+  //default: T.bool,
+  exact: T.bool,
+  component: T.element.isRequired
+}))
+
+function getRouteComponent(route) {
+  // todo validate route config
+
+  const props = {
+    key: route.path,
+    path: route.path,
+    exact: !!route.exact
+  }
+
+  if (route.routes) {
+    props.children = React.createElement(Switch, {}, route.routes.map(routeConfig => getRouteComponent(routeConfig)))
+  } else {
+    props.component = route.component
+  }
+
+  return React.createElement(Route, props)
+}
+
+/**
+ * Creates react router components based on config.
+ *
+ * Example of simple routing config :
+ *   [
+ *     {path: '',     component: MyComponent, exact: true},
+ *     {path: '/:id', component: MyOtherComponent}
+ *   ]
+ *
+ * Example of nested routing config :
+ *   [{
+ *     path: '/main',
+ *     routes: [
+ *       {path: '',     component: MyComponent, exact: true},
+ *       {path: '/:id', component: MyOtherComponent}
+ *     ]
+ *   }]
+ *
+ * @param {Array}  routesConfig
+ * @param {string} basePath
+ */
+export function routedApp(routesConfig, basePath = '') {
+  //checkPropTypes(routesTypes, routesConfig)
+
+  return () => React.createElement(Router, {
+    history: hashHistory
+  }, React.createElement(Route, {
+    path: basePath
+  }, React.createElement(Switch, {}, routesConfig.map(route => getRouteComponent(route)))))
+}
